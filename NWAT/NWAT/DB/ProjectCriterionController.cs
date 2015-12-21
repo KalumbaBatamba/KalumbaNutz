b5f6ea8359a9fa7b7018e4d8245998291fa000ac
Josh_Frey <kalumba81@googlemail.com>
2015-12-21 23:24:51 +0100

    ProjectCriterion Controller erweitert

--- NWAT/NWAT/DB/ProjectCriterionController.cs
+++ NWAT/NWAT/DB/ProjectCriterionController.cs
@@ -23,13 +23,45 @@ namespace NWAT.DB
         /// An instance of 'ProjectCriterion' 
         /// </returns>
         /// Erstellt von Joshua Frey, am 18.12.2015
-        public ProjectCriterion GetProjectCriterionByIds(NWATDataContext dataContext,  int projectId, int criterionId)
+        public ProjectCriterion GetProjectCriterionByIds(int projectId, int criterionId)
         {
-
-            ProjectCriterion resultProjectCriterion = dataContext.ProjectCriterion.SingleOrDefault(projectCriterion => projectCriterion.Criterion_Id == criterionId 
+            ProjectCriterion resultProjectCriterion = base.DataContext.ProjectCriterion.SingleOrDefault(projectCriterion => projectCriterion.Criterion_Id == criterionId 
                                                                               && projectCriterion.Project_Id == projectId);
             return resultProjectCriterion;
         }
+
+        /// <summary>
+        /// Gets all project criterions for one project.
+        /// </summary>
+        /// <param name="projectId">The project identifier.</param>
+        /// <returns>
+        /// A list of all project criterions of one project
+        /// </returns>
+        /// Erstellt von Joshua Frey, am 21.12.2015
+        public List<ProjectCriterion> GetAllProjectCriterionsForOneProject(int projectId)
+        {
+            List<ProjectCriterion> resultProjectCriterions = base.DataContext.ProjectCriterion.Where(projectCriterion => projectCriterion.Project_Id == projectId).ToList();
+            return resultProjectCriterions;
+        }
+
+        /// <summary>
+        /// Gets the child criterions by parent identifier.
+        /// </summary>
+        /// <param name="projectId">The project identifier.</param>
+        /// <param name="parentId">The parent identifier.</param>
+        /// <returns>
+        /// A list with all project criterions where project id = given project id and parent id = given parent id
+        /// </returns>
+        /// Erstellt von Joshua Frey, am 21.12.2015
+        public List<ProjectCriterion> GetChildCriterionsByParentId(int projectId, int parentId)
+        {
+            List<ProjectCriterion> resultProjectCriterions = base.DataContext.ProjectCriterion.Where(projectCriterion => projectCriterion.Parent_Criterion_Id == parentId
+                                                                                                     && projectCriterion.Project_Id == projectId).ToList();
+            return resultProjectCriterions;
+        }
+
+
+
 
 
     }
