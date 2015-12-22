24c8bcb2887fda82ef6bfbf641eaecd6a824f9de
Josh_Frey <kalumba81@googlemail.com>
2015-12-22 01:29:33 +0100

    Projectkriterium Controller erweitert --> noch in Arbeit

--- NWAT/NWAT/DB/ProjectCriterionController.cs
+++ NWAT/NWAT/DB/ProjectCriterionController.cs
@@ -60,9 +60,142 @@ namespace NWAT.DB
             return resultProjectCriterions;
         }
 
+        /// <summary>
+        /// Inserts the project criterion into database.
+        /// </summary>
+        /// <param name="newProjectCriterion">The new project criterion.</param>
+        /// <returns>
+        /// bool if insertion was sucessfully
+        /// </returns>
+        /// Erstellt von Joshua Frey, am 22.12.2015
+        /// <exception cref="DatabaseException">
+        /// </exception>
+        public bool InsertProjectCriterionIntoDb(ProjectCriterion newProjectCriterion)
+        {
+            if (newProjectCriterion != null)
+            {
+                int projectId = newProjectCriterion.Project_Id;
+                int criterionId = newProjectCriterion.Criterion_Id;
+
+                // set equal weighting
+                newProjectCriterion.Weighting_Cardinal = 1;
+
+                ProjectCriterion alreadyExistingProjectCriterion = this.GetProjectCriterionByIds(projectId, criterionId);
+
+                // if == null then this project criterion does not exist yet and it can be inserted into db
+                if (alreadyExistingProjectCriterion == null)
+                {
+                    base.DataContext.ProjectCriterion.InsertOnSubmit(newProjectCriterion);
+                    base.DataContext.SubmitChanges();
+                }
+
+                // project criterion already exists --> throw exception
+                else
+                {
+                    string projectName = alreadyExistingProjectCriterion.Project.Name;
+                    string criterionName = alreadyExistingProjectCriterion.Criterion.Name;
+                    throw (new DatabaseException((MessageProjectCriterionAlreadyExists(projectName, criterionName))));
+                }
+                return checkIfEqualProjectCriterions(newProjectCriterion, this.GetProjectCriterionByIds(projectId, criterionId));   
+            }
+            else
+            {
+                throw (new DatabaseException(MessageProjectCriterionCouldNotBeSavedEmptyObject()));        
+            }
+        }
 
 
+        /*
+        * Private Section
+        */
 
+
+        /// <summary>
+        /// Checks if equal project criterions.
+        /// </summary>
+        /// <param name="projectCriterionOne">The project criterion one.</param>
+        /// <param name="projectCriterionTwo">The project criterion two.</param>
+        /// <returns>
+        /// bool if same project criterions
+        /// </returns>
+        /// Erstellt von Joshua Frey, am 22.12.2015
+        private bool checkIfEqualProjectCriterions(ProjectCriterion projectCriterionOne, ProjectCriterion projectCriterionTwo)
+        {
+            bool sameProjectId = projectCriterionOne.Project_Id == projectCriterionTwo.Project_Id;
+            bool sameCriterionId = projectCriterionOne.Criterion_Id == projectCriterionTwo.Criterion_Id;
+            bool sameCardinalWeighting = projectCriterionOne.Weighting_Cardinal == projectCriterionTwo.Weighting_Cardinal;
+            bool samePercentageLayerWeighting = projectCriterionOne.Weighting_Percentage_Layer == projectCriterionTwo.Weighting_Percentage_Layer;
+            bool samePercentageProjectWeighting = projectCriterionOne.Weighting_Percentage_Project == projectCriterionTwo.Weighting_Percentage_Project;
+
+            return sameProjectId &&
+                   sameCriterionId &&
+                   sameCardinalWeighting &&
+                   samePercentageLayerWeighting &&
+                   samePercentageProjectWeighting;
+        }
+
+        /// <summary>
+        /// Checks if project criterion already exists.
+        /// </summary>
+        /// <param name="projectId">The project identifier.</param>
+        /// <param name="criterionId">The criterion identifier.</param>
+        /// <returns>
+        /// bool if project criterion already exists
+        /// </returns>
+        /// Erstellt von Joshua Frey, am 22.12.2015
+        private bool checkIfProjectCriterionAlreadyExists(int projectId, int criterionId)
+        {
+            ProjectCriterion existingProjectCriterion = this.GetProjectCriterionByIds(projectId, criterionId);
+            if(existingProjectCriterion != null)
+                return true;
+            else
+                return false;
+        }
+        
+        /// <summary>
+        /// Checks if parent exists in project as project criteroin.
+        /// </summary>
+        /// <param name="projectId">The project identifier.</param>
+        /// <param name="parentCritId">The parent crit identifier.</param>
+        /// <returns>
+        /// bool which says if given parent is a projectCriterion
+        /// </returns>
+        /// Erstellt von Joshua Frey, am 22.12.2015
+        private bool checkIfParentExistsInProjectAsProjectCriteroin(int projectId, int parentCritId)
+        {
+            ProjectCriterion resultProjectCriterion = base.DataContext.ProjectCriterion.FirstOrDefault(projectCriterion => projectCriterion.Project_Id == projectId && projectCriterion.Criterion_Id == parentCritId);
+            if (resultProjectCriterion == null)
+                return false;
+            else
+                return true;
+        }
+
+
+        /*
+         Messages
+         */
+        private string MessageProjectCriterionCouldNotBeSavedEmptyObject()
+        {
+            return "Das Projektkriterium konnte nicht in der Datenbank gespeichert werden." +
+                   " Bitte überprüfen Sie das übergebene Projektkriterium Objekt.";
+        }
+
+        private string MessageProjectCriterionAlreadyExists(string projectName, string criterionName)
+        {
+            return "Das Kriterium mit dem Namen \"" + criterionName +
+                   "\" ist bereits dem Projekt \"" + projectName + "\" zugeordnet";
+        }
+
+        //private string MessageCriterionDoesNotExist(int criterionId)
+        //{
+        //    return "Das Kriterium mit der Id \"" + criterionId +
+        //           "\" existiert nicht in der Datenbank.";
+        //}
+
+        //private string MessageCriterionHasNoId()
+        //{
+        //    return "Das Criterion Object besitzt keine ID. Bitte überprüfen Sie das übergebene Object";
+        //}
 
     }
 }
