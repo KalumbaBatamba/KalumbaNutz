b60397e959c6ac6144899cf253fbb5fc41ec2320
Josh_Frey <kalumba81@googlemail.com>
2016-01-02 11:56:04 +0100

    Analysis Class erstellt

---
+++ NWAT/NWAT/Analysis.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NWAT.DB;
+
+namespace NWAT
+{
+    class Analysis
+    {
+
+        public void Analyse()
+        {
+            // this instance of project criterion controller is needed to work with the Project_Criterion table
+            ProjectCriterionController projCritController = new ProjectCriterionController();
+
+            // This method will return all project ctiterions for one project
+            int projectId = 0;
+            List<ProjectCriterion> projectCriterions = projCritController.GetAllProjectCriterionsForOneProject(projectId);
+
+
+
+            
+        }
+    }
+}
