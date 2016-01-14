4353be7c71a976342e0f6f35ed929fd323a50b43
Adrianhsworms <inf1480@hs-worms.de>
2016-01-14 11:44:39 +0100

    Druckfunktion Projekte aus DB

--- NWAT/NWAT/Printer/ProjectDataPrinter.cs
+++ NWAT/NWAT/Printer/ProjectDataPrinter.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using NWAT.DB;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.IO;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
@@ -15,20 +20,95 @@ namespace NWAT.Printer
     /// 
     /// </summary>
     /// Erstellt von Adrian Glasnek
-    class ProjectDataPrinter
+    /// 
+
+    
+
+    public class ProjectDataPrinter
     {
-        Document doc;
 
-        public ProjectDataPrinter()
-        {
-            this.doc = new Document();
-        }
+        //Methode um allen in der Datenbank gelisteten Projekte in einer PDF auszugeben
 
-        public void printProjectData()
-        {
-            Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
-            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream("project.pdf", FileMode.Create));
+        public void ProjectDataPrint(){
+
+            ProjectController criContr = new ProjectController();
+            List<Project> x = criContr.GetAllProjectsFromDB();
+
+            //SaveFileDialog um Dokument am gewünschten Ort speicher zu können 
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Pdf File |*.pdf";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+
+                Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
+                PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(sfd.FileName, FileMode.Create));
+                doc.Open();
+
+                PdfPTable table = new PdfPTable(3);
+
+                //Überschrift setzen
+                Paragraph heading = new Paragraph("Projekte in Datenbank");
+
+                //Abstand nach Übersicht bis zur Tabelle
+                heading.SpacingAfter = 18f;
+
+                doc.Add(heading);
+
+
+
+
+                //Breite der einzelnen Zellen
+
+                table.TotalWidth = 316f;
+
+                //Lock von Table Breite
+
+                table.LockedWidth = true;
+
+
+
+                //Proportionen der Tabelle
+
+                float[] widths = new float[] { 1f, 2f, 3f };
+
+                table.SetWidths(widths);
+
+                table.HorizontalAlignment = 0;
+
+
+
+                //Lass Platz zwischen Tabelleneinträgen
+
+                table.SpacingBefore = 20f;
+
+                table.SpacingAfter = 30f;
+
+                //Überschriften für die Tabellenspalten
+
+                table.AddCell("ID");
+
+                table.AddCell("Projekt");
+
+                table.AddCell("Beschreibung");
+
+                //Schleife um Daten aus Datenbank zu holen
+
+                foreach (Project proj in x)
+                {
+
+                    table.AddCell(proj.Project_Id.ToString());
+                    table.AddCell(proj.Name.ToString());
+                    table.AddCell(proj.Description.ToString());
+
+                }
+                doc.Add(table);
+                doc.Close();
+
+                MessageBox.Show("PDF erfolgreich angelegt");
+                Application.Exit();
+            }
         }
+        
     }
 
 }
