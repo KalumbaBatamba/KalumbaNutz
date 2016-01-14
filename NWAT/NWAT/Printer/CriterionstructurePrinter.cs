5c295fb95d7622ccfa468b7107350d0e1c3a420a
Adrianhsworms <inf1480@hs-worms.de>
2016-01-14 13:38:07 +0100

    KriterienstrukturPrinter "Test"

--- NWAT/NWAT/Printer/CriterionstructurePrinter.cs
+++ NWAT/NWAT/Printer/CriterionstructurePrinter.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using NWAT.DB;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.IO;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
@@ -11,27 +16,109 @@ using iTextSharp.text.pdf;
 /// 
 /// </summary>
 /// Erstellt von Adrian Glasnek
+/// 
+
+
 namespace NWAT.Printer
 {
-    class CriterionstructurePrinter
+    public class CriterionStructurePrinter
     {
-        Document doc;
 
-        public CriterionstructurePrinter()
+        //Methode um allen in der Datenbank gelisteten Projekte in einer PDF auszugeben
 
+        public void CritStructPrinter()
         {
-        
-        this.doc = new Document();
-        
+
+            ProjectCriterionController cont = new ProjectCriterionController();
+            List<ProjectCriterion> projCrits = cont.GetAllProjectCriterionsForOneProject(1);
+
+            //SaveFileDialog um Dokument am gewünschten Ort speicher zu können 
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Pdf File |*.pdf";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+
+                Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
+                PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(sfd.FileName, FileMode.Create));
+                doc.Open();
+
+                PdfPTable table = new PdfPTable(2);
+
+                //Überschrift setzen
+
+                
+                Paragraph heading = new Paragraph("Kriterienstruktur");
+
+                //Abstand nach Übersicht bis zur Tabelle
+                heading.SpacingAfter = 18f;
+
+
+
+                //ProjectCriterionController cont = new ProjectCriterionController();
+                //ProjectCriterion crit = cont.GetProjectCriterionByIds(2, 2);
+
+                //int layer = crit.Layer_Depth;
+                //double intendFactor = 10;
+                //double intend = layer * intendFactor;
+
+                //heading.IndentationLeft = (Convert.ToSingle(intend));
+
+                doc.Add(heading);
+
+
+
+
+                //Breite der einzelnen Zellen
+
+                table.TotalWidth = 316f;
+
+                //Lock von Table Breite
+
+                table.LockedWidth = true;
+
+
+
+                //Proportionen der Tabelle
+
+                float[] widths = new float[] { 1f, 2f };
+
+                table.SetWidths(widths);
+
+                table.HorizontalAlignment = 0;
+
+
+
+                //Lass Platz zwischen Tabelleneinträgen
+
+                table.SpacingBefore = 20f;
+
+                table.SpacingAfter = 30f;
+
+                //Überschriften für die Tabellenspalten
+
+                table.AddCell("Projekt ID");
+
+                table.AddCell("Kriterium");
+
+               
+
+                //Schleife um Daten aus Datenbank zu holen
+
+                foreach (ProjectCriterion proj in projCrits)
+                {
+
+                    table.AddCell(proj.Project_Id.ToString());
+                    table.AddCell(proj.Criterion.Description.ToString());
+
+                }
+                doc.Add(table);
+                doc.Close();
+
+                MessageBox.Show("PDF erfolgreich angelegt");
+
+                Application.Exit();
+            }
         }
 
-        public void printCriterionForCostumer()
-        {
-        
-            Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
-            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream("CostumerCrit.pdf", FileMode.Create));
-           
-
-        }
     }
 }
