5c499aa3dcc7c6d74a8f40dcfdaae18d5b569ba6
inf2449 <inf2449@hs-worms.de>
2016-01-20 00:45:47 +0100

    Analysis update with to do list

--- NWAT/NWAT/Analysis.cs
+++ NWAT/NWAT/Analysis.cs
@@ -22,7 +22,8 @@ namespace NWAT
 
 
 
-        // TODO By Yann
+        // TODO By Yann 
+        // calculate the fullfilment level of each product and generate a list to print.
         public void Analyse()
         {
             // this instance of project criterion controller is needed to work with the Project_Criterion table
@@ -39,8 +40,17 @@ namespace NWAT
             // this method will return all project Criterions, which don't have any parent_id --> so the base criterions will be returned
             List<ProjectCriterion> baseCriterions = projCritController.GetBaseProjectCriterions(projectId);
 
+            foreach(ProjectCriterion papa in baseCriterions)
+            foreach(ProjectCriterion son in projectCriterions)
+            {
+                Fulfillment fullcon = new Fulfillment();
+                if (fullcon.Criterion_Id.Equals(son.Criterion_Id) && fullcon.Fulfilled == true)
+                {
+                    
+                }
+            }
 
-
+            return ;
 
         }
     }
@@ -56,10 +66,34 @@ namespace NWAT
         private double projectPercentageWeighting;
         List<ProductCriterionFulfillmentResult> productResults;
 
-        public AnalysisCriterionResult(String critName)
+        public AnalysisCriterionResult()
         {
-            this.critName = critName;
+           /// this.critName = critName;
+           // this.critDescription = critDescription;
+            //this.cardinalWeighting = cardinalWeighting;
+           // this.layerPercentageWeighting = layerPercentageWeighting;
+           // this.projectPercentageWeighting = projectPercentageWeighting;
+            
+
         }
+
+        //Generate a list of all criterions for the view of the analysis.
+        public void Analyseresult()
+        {
+            int projectId = 0;
+
+            ProjectCriterionController projCritController = new ProjectCriterionController();
+            ProjectProductController projprodContr = new ProjectProductController();
+            List<ProjectProduct> projprod = projprodContr.GetAllProjectProductsForOneProject(projectId);
+            List<ProjectCriterion> pjcrit = projCritController.GetBaseProjectCriterions(projectId);
+            List<ProjectCriterion> critall = projCritController.GetAllProjectCriterionsForOneProject(projectId);
+            List<ProductCriterionFulfillmentResult> Listtoshows = new List<ProductCriterionFulfillmentResult>();
+            AnalysisCriterionResult Anacritresult = new AnalysisCriterionResult();
+           
+            // To do write the algo to generate a list of each product with all information of their criterions and their fulfillment
+            
+        }
+
 
 
     }
@@ -69,34 +103,37 @@ namespace NWAT
     // Class for the list of each Product with their respective Criterion and their fulfillment
     class ProductCriterionFulfillmentResult
     {
+        
         int criterionId;
         string productName;
-        double result;
+        float result;
         int projectId = 0;
         String criterionName;
 
-
-
-        public ProductCriterionFulfillmentResult(string productName, int criterionId, String criterionName, double result)
+        //Constructor of the class to generate the list of product and criterion
+        public ProductCriterionFulfillmentResult(string productName, int criterionId, String criterionName, float result)
         {
             this.productName = productName;
             this.result = result;
             this.criterionId = criterionId;
             this.criterionName = criterionName;
 
+
         }
 
+        //calculate the Weighting_Percentage_Project for each parentcriterion and generate a list of each product with their respective  Weighting_Percentage_Project
+        
         public List<ProductCriterionFulfillmentResult> Listtoshow()
         {
-            //calculate the Weighting_Percentage_Project for each parentcriterion and generate a list of each product with their respective  Weighting_Percentage_Project
-
-
+            
 
             ProjectCriterionController projCritController = new ProjectCriterionController();
             ProjectProductController projprodContr = new ProjectProductController();
             List<ProjectProduct> projprod = projprodContr.GetAllProjectProductsForOneProject(projectId);
             List<ProjectCriterion> pjcrit = projCritController.GetBaseProjectCriterions(projectId);
             List<ProjectCriterion> critall = projCritController.GetAllProjectCriterionsForOneProject(projectId);
+            List<ProductCriterionFulfillmentResult> Listtoshows = new List<ProductCriterionFulfillmentResult>();
+
 
             foreach (ProjectCriterion allcrit in critall)
                 foreach (ProjectProduct prod in projprod)
@@ -107,7 +144,7 @@ namespace NWAT
 
                             result += pcrit.Weighting_Percentage_Project;
 
-                            List<ProductCriterionFulfillmentResult> Listtoshows = new List<ProductCriterionFulfillmentResult>();
+                            
 
                             ProductCriterionFulfillmentResult pf = new ProductCriterionFulfillmentResult(prod.Product.Name, pcrit.Criterion.Criterion_Id, pcrit.Criterion.Name, result);
                             Listtoshows.Add(pf);
@@ -116,7 +153,7 @@ namespace NWAT
                         
 
                     }
-
+          
             
         }
 
