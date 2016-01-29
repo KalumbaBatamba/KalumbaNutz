9e4e1c7f84e1cfabfa1f77d2a6d037348e2946ee
Josh_Frey <kalumba81@googlemail.com>
2016-01-29 01:58:44 +0100

    res

--- NWAT/NWAT/Views/ProjProdAssign_View.Designer.cs
+++ NWAT/NWAT/Views/ProjProdAssign_View.Designer.cs
@@ -46,41 +46,37 @@
             this.dataGridView_prodAvail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView_prodAvail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_prodAvail.Location = new System.Drawing.Point(716, 48);
-            this.dataGridView_prodAvail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView_prodAvail.Location = new System.Drawing.Point(365, 31);
             this.dataGridView_prodAvail.Name = "dataGridView_prodAvail";
             this.dataGridView_prodAvail.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView_prodAvail.Size = new System.Drawing.Size(650, 754);
+            this.dataGridView_prodAvail.Size = new System.Drawing.Size(252, 324);
             this.dataGridView_prodAvail.TabIndex = 11;
             // 
             // label_ProdAvail
             // 
             this.label_ProdAvail.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_ProdAvail.AutoSize = true;
-            this.label_ProdAvail.Location = new System.Drawing.Point(1208, 23);
-            this.label_ProdAvail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_ProdAvail.Location = new System.Drawing.Point(512, 15);
             this.label_ProdAvail.Name = "label_ProdAvail";
-            this.label_ProdAvail.Size = new System.Drawing.Size(157, 20);
+            this.label_ProdAvail.Size = new System.Drawing.Size(107, 13);
             this.label_ProdAvail.TabIndex = 9;
             this.label_ProdAvail.Text = "verfügbare Produkte:";
             // 
             // label_ProjProd
             // 
             this.label_ProjProd.AutoSize = true;
-            this.label_ProjProd.Location = new System.Drawing.Point(22, 15);
-            this.label_ProjProd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_ProjProd.Location = new System.Drawing.Point(15, 10);
             this.label_ProjProd.Name = "label_ProjProd";
-            this.label_ProjProd.Size = new System.Drawing.Size(171, 20);
+            this.label_ProjProd.Size = new System.Drawing.Size(115, 13);
             this.label_ProjProd.TabIndex = 8;
             this.label_ProjProd.Text = "zugeordnete Produkte:";
             // 
             // btn_ProjProdCancle
             // 
             this.btn_ProjProdCancle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_ProjProdCancle.Location = new System.Drawing.Point(1253, 811);
-            this.btn_ProjProdCancle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_ProjProdCancle.Location = new System.Drawing.Point(542, 361);
             this.btn_ProjProdCancle.Name = "btn_ProjProdCancle";
-            this.btn_ProjProdCancle.Size = new System.Drawing.Size(112, 35);
+            this.btn_ProjProdCancle.Size = new System.Drawing.Size(75, 23);
             this.btn_ProjProdCancle.TabIndex = 15;
             this.btn_ProjProdCancle.Text = "schliessen";
             this.btn_ProjProdCancle.UseVisualStyleBackColor = true;
@@ -89,10 +85,9 @@
             // btn_ProjProdSave
             // 
             this.btn_ProjProdSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_ProjProdSave.Location = new System.Drawing.Point(1131, 811);
-            this.btn_ProjProdSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_ProjProdSave.Location = new System.Drawing.Point(461, 361);
             this.btn_ProjProdSave.Name = "btn_ProjProdSave";
-            this.btn_ProjProdSave.Size = new System.Drawing.Size(112, 35);
+            this.btn_ProjProdSave.Size = new System.Drawing.Size(75, 23);
             this.btn_ProjProdSave.TabIndex = 14;
             this.btn_ProjProdSave.Text = "speichern";
             this.btn_ProjProdSave.UseVisualStyleBackColor = true;
@@ -101,10 +96,9 @@
             // btn_ProdToPool
             // 
             this.btn_ProdToPool.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_ProdToPool.Location = new System.Drawing.Point(588, 381);
-            this.btn_ProdToPool.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_ProdToPool.Location = new System.Drawing.Point(279, 201);
             this.btn_ProdToPool.Name = "btn_ProdToPool";
-            this.btn_ProdToPool.Size = new System.Drawing.Size(90, 35);
+            this.btn_ProdToPool.Size = new System.Drawing.Size(60, 23);
             this.btn_ProdToPool.TabIndex = 13;
             this.btn_ProdToPool.Text = ">>";
             this.btn_ProdToPool.UseVisualStyleBackColor = true;
@@ -113,10 +107,9 @@
             // btn_ProdToProj
             // 
             this.btn_ProdToProj.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_ProdToProj.Location = new System.Drawing.Point(588, 336);
-            this.btn_ProdToProj.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_ProdToProj.Location = new System.Drawing.Point(279, 162);
             this.btn_ProdToProj.Name = "btn_ProdToProj";
-            this.btn_ProdToProj.Size = new System.Drawing.Size(90, 35);
+            this.btn_ProdToProj.Size = new System.Drawing.Size(60, 23);
             this.btn_ProdToProj.TabIndex = 12;
             this.btn_ProdToProj.Text = "<<";
             this.btn_ProdToProj.UseVisualStyleBackColor = true;
@@ -128,30 +121,30 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView_ProjProd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_ProjProd.Location = new System.Drawing.Point(27, 48);
-            this.dataGridView_ProjProd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView_ProjProd.Location = new System.Drawing.Point(18, 31);
             this.dataGridView_ProjProd.MultiSelect = false;
             this.dataGridView_ProjProd.Name = "dataGridView_ProjProd";
             this.dataGridView_ProjProd.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView_ProjProd.Size = new System.Drawing.Size(525, 754);
+            this.dataGridView_ProjProd.Size = new System.Drawing.Size(243, 324);
             this.dataGridView_ProjProd.TabIndex = 10;
             // 
             // label_entkoppeln
             // 
             this.label_entkoppeln.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label_entkoppeln.AutoSize = true;
-            this.label_entkoppeln.Location = new System.Drawing.Point(13, 831);
+            this.label_entkoppeln.Location = new System.Drawing.Point(9, 374);
+            this.label_entkoppeln.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_entkoppeln.Name = "label_entkoppeln";
-            this.label_entkoppeln.Size = new System.Drawing.Size(634, 20);
+            this.label_entkoppeln.Size = new System.Drawing.Size(425, 13);
             this.label_entkoppeln.TabIndex = 16;
             this.label_entkoppeln.Text = "Achtung: Beim Entkoppeln werden auch die Einträge aus der Fulfillment Tabelle gel" +
     "öscht";
             // 
             // ProjProdAssign_View
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1387, 863);
+            this.ClientSize = new System.Drawing.Size(632, 395);
             this.Controls.Add(this.label_entkoppeln);
             this.Controls.Add(this.dataGridView_prodAvail);
             this.Controls.Add(this.dataGridView_ProjProd);
@@ -161,7 +154,6 @@
             this.Controls.Add(this.btn_ProjProdSave);
             this.Controls.Add(this.btn_ProdToPool);
             this.Controls.Add(this.btn_ProdToProj);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ProjProdAssign_View";
             this.Text = "Projektproduktzuordnung";
             this.Load += new System.EventHandler(this.ProjProdAssign_Form_Load);
